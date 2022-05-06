2e44065a8a4903f9e37514515658aef46bcf24a6
nicolasjesse <nicolasjesseam@gmail.com>
2022-05-06 05:47:35 -0300

    add flash messages to warehouse create

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,24 +1,19 @@
 class WarehousesController < ApplicationController
-    def show
-        @warehouse = Warehouse.find(params[:id])
-    end
+  def show
+    @warehouse = Warehouse.find(params[:id])
+  end
 
-    def new
-        @warehouse = Warehouse.new
-    end
-
-    def create
-        @warehouse = Warehouse.new(name: params[:warehouse][:name],
-            description: params[:warehouse][:description],
-            code: params[:warehouse][:code],
-            city: params[:warehouse][:city],
-            area: params[:warehouse][:area],
-            address: params[:warehouse][:address],
-            CEP: params[:warehouse][:CEP],
-            )
+  def new
+    @warehouse = Warehouse.new
+  end
+  
+  def create
+    @warehouse = Warehouse.new(params.require(:warehouse)
+                          .permit(:name, :code, :city, :description, :address, :cep, :area))
     if @warehouse.save
-        return redirect_to root_path
+      flash[:notice] = "Galpão cadastrado com sucesso."
+      return redirect_to root_path
     end
     render :new
-    end
+  end
 end
