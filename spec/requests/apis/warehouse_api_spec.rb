25cbab1cff2d7b8e5febd710ec146eb6e8008235
nicolasjesse <nicolasjesseam@gmail.com>
2022-06-08 06:14:31 -0300

    add get warehouses endpoint

--- spec/requests/apis/warehouse_api_spec.rb
+++ spec/requests/apis/warehouse_api_spec.rb
@@ -16,6 +16,42 @@ describe 'Warehouse API' do
 
       expect(json_response["name"]).to eq 'Aeroporto SP'
       expect(json_response["code"]).to eq 'GRU'
+      expect(json_response.keys).not_to include 'created_at'
+      expect(json_response.keys).not_to include 'updated_at'
+    end
+
+    it 'fail if warehouse not found' do
+      get '/api/v1/warehouses/9999999'
+
+      expect(response.status).to eq 404
+    end
+  end
+
+  context 'GET /api/v1/warehouses' do
+    it 'success' do
+      Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                                    address: 'Avenida do Aeroporto, 1000', CEP: '15000-000',
+                                    description: 'Galpão destinado para cargas internacionais')
+
+      Warehouse.create!(name: 'Aeroporto RJ', code: 'RJA', city: 'Rio', area: 40_000,
+                                      address: 'Avenida Copacabana, 4000', CEP: '25000-000',
+                                      description: 'Galpão do rio')
+
+      get '/api/v1/warehouses'
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+      json_response = JSON.parse(response.body)
+      expect(json_response.length).to eq 2
+    end
+
+    it 'return empty if theres no warehouse' do
+      get '/api/v1/warehouses'
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+      json_response = JSON.parse(response.body)
+      expect(json_response).to eq []
     end
   end
 end
